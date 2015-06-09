ec0522dcacd11f4991d37d4918ba14fdb5d7fe79
Pierre Hyvernat <pierre.hyvernat@univ-savoie.fr>
2015-06-09 13:53:11 +0200

    bug in explore function

--- explore.ml
+++ explore.ml
@@ -6,15 +6,15 @@ open Typing
 
 let print_term_depth (env:environment) (v:term) (depth:int) : unit
   =
-  (* NOTE: not very elegant: I pasted the "print_term" function and added some things... *)
+  (* NOTE: not very elegant: I pasted the "print_term_depth_aux" function and added some things... *)
     let rec
       print_paren_term v depth =
         try
             print_term_int v
         with Invalid_argument "print_term_int" ->
             if is_atomic v
-            then print_term v depth
-            else (print_string "("; print_term v depth; print_string ")")
+            then print_term_depth_aux v depth
+            else (print_string "("; print_term_depth_aux v depth; print_string ")")
 
     and
       print_non_codata_term u depth = match u with
@@ -22,10 +22,10 @@ let print_term_depth (env:environment) (v:term) (depth:int) : unit
         | Var(x) -> print_string x
         | Const(c,p) -> print_string c; print_exp p
         | Proj(d,p) -> print_string "." ; print_string d; print_exp p
-        | App(v1,v2) -> print_term v1 depth; print_string " "; print_paren_term v2 depth
+        | App(v1,v2) -> print_term_depth_aux v1 depth; print_string " "; print_paren_term v2 depth
 
     and
-      print_term v depth =
+      print_term_depth_aux v depth =
         try
             print_term_int v
         with Invalid_argument "print_term_int" ->
@@ -36,15 +36,21 @@ let print_term_depth (env:environment) (v:term) (depth:int) : unit
                         begin
                             let p = get_type_priority env t in
                             if p mod 2 = 1
-                            then print_term v depth
+                            then print_non_codata_term v depth
                             else
                                 if depth = 0 then print_string "..."
                                 else
                                     let consts = get_type_constants env t in
-                                    let fields = List.map (fun d -> d, reduce_all env (App(Proj(d,p),v))) consts in
-                                    print_list "{}" "{" "; " "}" (function d,v -> print_string (d ^ "="); print_term v (depth-1)) fields
+                                    let fields = List.map (fun d ->
+                                        let t = App(Proj(d,p),v) in
+                                        (* print_string "reducing "; print_term t; print_string " = "; flush_all(); *)
+                                        let nf = reduce_all env t in
+                                        (* print_term nf; print_newline(); flush_all(); *)
+                                        (d, nf)) consts
+                                    in
+                                    print_list "{}" "{" "; " "}" (function d,v -> print_string (d ^ "="); print_term_depth_aux v (depth-1); flush_all()) fields
                         end
             end
     in
-    print_term v depth
+    print_term_depth_aux v depth
 
