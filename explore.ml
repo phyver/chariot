b7c812a304968ab3afa44e1b0f904ae5c2b38fbb
Pierre Hyvernat <pierre.hyvernat@univ-savoie.fr>
2015-06-18 15:28:02 +0200

    better handling of exploration of codata

--- explore.ml
+++ explore.ml
@@ -9,44 +9,61 @@ type explore_struct = Folded of int * term * type_expression | Unfolded of (cons
 
 let rec
    print_explore_struct = function
-       | Folded(n,v,t) -> print_string "…"; print_int n; print_string "…:"; print_type t
+       | Folded(n,v,t) -> print_string "…"; print_int n; print_string "…:"
        | Unfolded fields -> print_list "{}" "{" "; " "}" (function d,v -> print_string (d ^ "="); print_explore_term v) fields
 and
   print_explore_term v = print_special_term print_explore_struct v
 
-let rec term_to_explore (v:term) : explore_term = match v with
+let rec head_to_explore (v:term) : explore_term = match v with
     | Angel -> Angel
     | Var x -> Var x
     | Proj(d,p) -> Proj(d,p)
     | Const(c,p) -> Const(c,p)
-    | App(v1,v2) -> App(term_to_explore v1,term_to_explore v2)
     | Special v -> v.bot
+    | App(v1,v2) -> assert false
 
-let unfold (env:environment) (v:term) (depth:int) : explore_term
- =
-    let rec unfold_aux depth (v:term) : explore_term =
-        let hd, args = get_head v, get_args v in
-        match infer_type env v [] [] with
-            | Arrow _,_,_ | TVar _,_,_ -> app (term_to_explore hd) (List.map (unfold_aux depth) args)
-            | Data(tname,_) as t,_,_ ->
+let struct_nb = ref 0
+
+let rec term_to_explore (env:environment) (v:term) : explore_term
+ =  let hd, args = get_head v, get_args v in
+    match infer_type env v [] [] with
+        | Data(tname,_) as t,_,_ ->
+            if (is_inductive env tname)
+            then
+                app (head_to_explore hd) (List.map (term_to_explore env) args)
+            else
+                (incr struct_nb; Special (Folded (!struct_nb,v,t)))
+        | Arrow _,_,_ | TVar _,_,_ ->
+            app (head_to_explore hd) (List.map (term_to_explore env) args)
+let term_to_explore env v = struct_nb:=0; term_to_explore env v
+
+
+let rec unfold (env:environment) (p:int->bool) (v:explore_term) : explore_term
+ =  match v with
+        | Angel | Var _ | Proj _ | Const _ -> v
+        | App(v1,v2) -> App(unfold env p v1, unfold env p v2)
+        | Special(Unfolded fields) -> Special (Unfolded (List.map (second (unfold env p)) fields))
+        | Special(Folded(n,v,t)) when not (p n) -> incr struct_nb; Special(Folded(!struct_nb,v,t))
+        | Special(Folded(n,v,Data(tname,_))) when (p n) ->
                 let p = get_type_priority env tname in
-                if p mod 2 = 1
-                then app (term_to_explore hd) (List.map (unfold_aux depth) args)
-                else
-                    if depth = 0
-                    then Special (Folded (0,app hd args,t))
-                    else
-                        let consts = get_type_constants env tname in
-                        let fields = List.map (fun d ->
-                            let t = App(Proj(d,Some p),v) in
-                            let nf = unfold_aux (depth-1) (reduce_all env t) in
-                            (d, nf)) consts
-                        in
-                        Special (Unfolded fields)
+                let consts = get_type_constants env tname in
+                let fields = List.map (fun d ->
+                    let v = App(Proj(d,Some p),v) in
+                    let v = reduce_all env v in
+                    (d, term_to_explore env v)) consts
+                in
+                Special (Unfolded fields)
+        | Special _ -> assert false
 
-    in unfold_aux depth (reduce_all env v)
+let unfold env p v = struct_nb:=0; unfold env p v
 
+let rec unfold_to_depth env v depth
+ =  if depth = 0
+    then term_to_explore env v
+    else
+        let v = unfold_to_depth env v (depth-1) in
+        unfold env (fun _ -> true) v
 
 let explore_term_depth (env:environment) (v:term) (depth:int) : unit
-  = print_explore_term (unfold env v depth)
+  = print_explore_term (unfold_to_depth env v depth)
 
