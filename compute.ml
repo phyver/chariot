b45870f64e5bd76d864a4e47cffdf94bdc76e68b
Pierre Hyvernat <pierre.hyvernat@univ-savoie.fr>
2015-06-27 14:44:24 +0200

    bug in compute
    I need to compare terms up-to priorities...

--- compute.ml
+++ compute.ml
@@ -8,6 +8,13 @@ let rec subst_term sigma (v:term) = match v with
     | App(v1,v2) -> App(subst_term sigma v1, subst_term sigma v2)
     | Special v -> v.bot
 
+let rec equal_term v1 v2 = match v1,v2 with
+    | Var(x),Var(y) -> x=y
+    | Angel,Angel -> true
+    | App(v11,v12),App(v21,v22) -> (equal_term v11 v21) && (equal_term v12 v22)
+    | Proj(d1,_),Proj(d2,_) -> d1=d2
+    | Const(c1,_),Const(c2,_) -> c1=c2
+    | _,_ -> false
 
 let unify_pattern (pattern,def:term*term) (v:term) : term
   = (* the function defined by the pattern: this variable cannot be instantiated! *)
@@ -16,7 +23,7 @@ let unify_pattern (pattern,def:term*term) (v:term) : term
     let rec unify_aux (eqs:(term*term) list) acc =
         match eqs with
             | [] -> acc
-            | (s,t)::eqs when s=t -> unify_aux eqs acc
+            | (s,t)::eqs when equal_term s t -> unify_aux eqs acc
             | (App(u1,v1),App(u2,v2))::eqs -> unify_aux ((u1,u2)::(v1,v2)::eqs) acc
             | (Var _f, _)::_ when _f = f -> unificationError "cannot unify the function name"
             | (Var x, v)::eqs ->
@@ -53,7 +60,7 @@ let reduce_all (env:environment) (v:term) : term
                 let v1,b1 = reduce v1 in
                 let v2,b2 = reduce v2 in
                 let v3,b3 = (try reduce_first_clause (App(v1,v2)) (get_function_clauses env (get_function_name v))
-                             with Invalid_argument "no head function" | Not_found -> App(v1,v2),false) in
+                             with Invalid_argument "no head function" | Not_found -> App(v1,v2),b1||b2) in
                 v3, b1||b2||b3
           | Special v -> v.bot
     in
