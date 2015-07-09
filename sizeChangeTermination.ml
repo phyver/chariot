3eeab13a9d53cb94e513a69a4b747b926f6fa5a5
Pierre Hyvernat <pierre.hyvernat@univ-savoie.fr>
2015-07-09 22:34:33 +0200

    some work on utility function for SCT

--- sizeChangeTermination.ml
+++ sizeChangeTermination.ml
@@ -9,10 +9,117 @@ let add_infty w1 w2 = match w1,w2 with
 
 let add_infty_int w n = add_infty w (Num n)
 
-type approx_term = (z_infty * priority * var_name) list special_term
+type sct_pattern =
+    | SCTVar of var_name
+    | SCTAngel
 
-type sct_clause = term * approx_term
+    | SCTConst of const_name * priority * sct_pattern list
+    | SCTConstApprox of (priority * z_infty * var_name) list
 
+    | SCTProj of const_name * priority
+    | SCTProjApprox of priority * z_infty
+
+(* a call from f to g is represented by a rewriting rule
+ *   param_1 param_2 ... param_m  =>  arg_1 arg_2 ... arg_n
+ * where m is the arity of f and n is the arity of g.
+ *  - each param_i is either a constructor pattern or a destructor
+ *  - each arg_i i either a constructor pattern (with possible approximations) or a destructor
+ *)
+type sct_lhs = sct_pattern list
+type sct_rhs = sct_pattern list
+type sct_clause = sct_lhs * sct_rhs
+
+
+let is_clause (lhs,rhs : sct_clause) : bool =
+    let rec is_constructor_pattern a s = function
+        | SCTVar _ -> true
+        | SCTAngel -> a
+        | SCTConst(_,_,ps) -> List.for_all (is_constructor_pattern a s) ps
+        | SCTConstApprox _ -> s
+        | SCTProj _ -> false
+        | SCTProjApprox _ -> false
+    in
+    let is_destructor = function
+        | SCTProj _ -> true
+        | SCTProjApprox _ -> true
+        | _ -> false
+    in
+    let check_lhs = List.for_all (fun p -> is_constructor_pattern false false p || is_destructor p)
+    in
+    let check_rhs = List.for_all (fun p -> is_constructor_pattern true true p || is_destructor p)
+    in
+    check_lhs lhs && check_rhs rhs
+
+
+let add_approx a1 a2 = match a1,a2 with
+    | (None,w1) , (None,w2) -> (None, add_infty w1 w2)
+    | (None,w) , _ | _,(None,w) -> (None, w)
+    | (Some p1, w1) , (Some p2, w2) when p1<p2 -> (Some p2, w2)
+    | (Some p1, w1) , (Some p2, w2) when p1>p2 -> (Some p1, w1)
+    | (Some p1, w1) , (Some p2, w2) (*when p1=p2*) -> (Some p1, add_infty w1 w2)
+
+let simplify_approx aps =
+    let aps = List.sort (fun t1 t2 -> let _,_,x1 = t1 and _,_,x2 = t2 in compare x1 x2) aps in
+    let rec simplify_aux = function
+        | [] -> []
+        | [x] -> [x]
+        | (p1,w1,x1)::(((_,_,x2)::_) as aps) when x1<x2 -> (p1,w1,x1)::(simplify_aux aps)
+        | (p1,w1,x1)::(p2,w2,x2)::aps (*when x1<x2*) ->
+                assert (x1=x2);
+                let p,w = add_approx (p1,w1) (p2,w2) in
+                simplify_aux ((p,w,x1)::aps)
+    in
+    simplify_aux aps
+
+
+let merge_approx as1 as2 =
+    let as1 = List.sort (fun t1 t2 -> let _,_,x1 = t1 and _,_,x2 = t2 in compare x1 x2) as1 in
+    let as2 = List.sort (fun t1 t2 -> let _,_,x1 = t1 and _,_,x2 = t2 in compare x1 x2) as2 in
+    let rec merge_aux as1 as2 = match as1,as2 with
+        | as1 , [] | [] , as1 -> as1
+        | (p1,w1,x1)::as1 , (_,_,x2)::_ when x1<x2 -> (p1,w1,x1)::(merge_aux as1 as2)
+        | (_,_,x1)::_ , (p2,w2,x2)::as2 when x1>x2 -> (p2,w2,x2)::(merge_aux as1 as2)
+        | (p1,w1,x1)::as1 , (p2,w2,x2)::as2 (*when x1=x2*) ->
+                let (p,w) = add_approx (p1,w1) (p2,w2) in
+                (p,w,x1)::(merge_aux as1 as2)
+    in
+    merge_aux as1 as2
+
+
+let collapse_rhs depth (rhs:sct_rhs) : sct_rhs =
+    let rec collapse0 p = match p with
+        | SCTVar x -> [ (Some 0, Num 0, x) ]
+        | SCTAngel -> assert false (* FIXME *)
+        | SCTConstApprox ap -> ap
+        | SCTProj _ | SCTProjApprox _ -> assert false
+        | SCTConst(_,prio,ps) ->
+            begin
+                let approx_s = List.map collapse0 ps in
+                let approx = List.fold_left (fun as1 as2 -> merge_approx as1 (simplify_approx as2)) [] approx_s in  (* NOTE: not necessary to simplify as1: it is the recursive call and is simplified *)
+                List.map (function (p,w,x) -> let p,w = add_approx (prio,Num 1) (p,w) in (p,w,x)) approx
+            end
+    in
+    let rec collapse_const d p =
+        if d=0
+        then SCTConstApprox (collapse0 p)
+        else
+            match p with
+        | SCTVar _ | SCTAngel | SCTConstApprox _ -> p
+        | SCTProj _ | SCTProjApprox _ -> assert false
+        | SCTConst(c,p,ps) -> SCTConst(c,p,List.map (collapse_const (d-1)) ps)
+    in
+    let rec collapse_rhs_aux depth ps = match ps with
+        | [] -> []
+        | SCTProj(p,d)::ps ->
+            if depth = 0
+            then assert false (* TODO...*)
+            else
+                SCTProj(p,d)::(collapse_rhs_aux (depth-1) ps)
+        | [SCTProjApprox _ ] -> ps
+        | (SCTProjApprox _)::_ -> assert false
+        | (SCTVar _ | SCTAngel | SCTConst _ | SCTConstApprox _) as p::ps -> (collapse_const depth p)::(collapse_rhs_aux depth ps)
+    in
+    collapse_rhs_aux depth rhs
 
 
 (* composition:
